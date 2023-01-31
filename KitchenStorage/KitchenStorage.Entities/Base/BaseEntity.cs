5611c986f579baad62b4deaae6691f24104ec056
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-01-31 20:50:51 +0330

    create base entity

---
+++ KitchenStorage/KitchenStorage.Entities/Base/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace KitchenStorage.Entities.Base;
+
+public record BaseEntity
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public DateTime CreateDate { get; set; }
+
+    [Required]
+    public byte Status { get; set; }
+}
+
+public enum EntityStatus
+{
+    Active = 0,
+    DeActive = 1,
+    Deleted = 2,
+}
