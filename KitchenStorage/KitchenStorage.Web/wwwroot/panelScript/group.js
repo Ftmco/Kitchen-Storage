e8e73230eb98e9d5befbe5922c207cfbf9bfa8fb
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-02 14:41:21 +0330

    create upsert group script functions

--- KitchenStorage/KitchenStorage.Web/wwwroot/panelScript/group.js
+++ KitchenStorage/KitchenStorage.Web/wwwroot/panelScript/group.js
@@ -4,3 +4,18 @@ const newGroup = () => {
         createModal('افزودن گروه جدید', await res.text())
     })
 }
+
+const createGroup = () => {
+    let name = document.getElementById('group-name')
+    upsertGroup({ id: null,name: name.value })
+}
+
+const upsertGroup = ({ id, name }) => {
+    fetch('/api/Group/Upsert', postJsonApiCallOptions({ 'id': id, 'name': name })).then(async (rest) => {
+        const body = await rest.json()
+        if (body.status) {
+            closeModal()            
+        }
+        showToast(body.title)
+    })
+}
