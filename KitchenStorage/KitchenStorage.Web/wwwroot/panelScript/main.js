f334c78a289e5eeb0f74476d131194bddc9aaf89
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-02 13:12:01 +0330

    create app static files
    1. create modal and add to layout
    2. create app custom css styles
    3. create main script file for do main actions

---
+++ KitchenStorage/KitchenStorage.Web/wwwroot/panelScript/main.js
@@ -0,0 +1,24 @@
+﻿const showToast = (message) => {
+    let x = document.getElementById("snackbar");
+    x.innerHTML = message;
+    x.className = "show";
+    setTimeout(() => { x.className = x.className.replace("show", ""); }, 3000);
+}
+
+const getHtmlApiCallOptions = {
+    method: 'GET',
+    headers: {
+        'content-type': 'text/html'
+    }
+}
+
+
+const showLoading = () => {
+    let ulLoaderul = document.getElementById('ulLoaderul');
+    ulLoaderul.style.display = 'flex';
+}
+
+const hideLoading = () => {
+    let ulLoaderul = document.getElementById('ulLoaderul');
+    ulLoaderul.style.display = 'none';
+}
--- KitchenStorage/KitchenStorage.Web/wwwroot/panelScript/modal.js
+++ KitchenStorage/KitchenStorage.Web/wwwroot/panelScript/modal.js
@@ -1,1 +1,33 @@
-﻿
+﻿const createUniqId = () => {
+    const myString = "1234567890qwertyuioplkjhgfdsazxcvbnm";
+    let uniqueId = [];
+    for (let i = 0; i < 30; i++) {
+        uniqueId.push(myString[Math.floor(Math.random() * myString.length)]);
+    }
+    return uniqueId.join("");
+}
+
+
+
+const createModal = () => {
+    let id = createUniqId()
+    let modalHtml = `<div id="${id}" class="w3-modal modal-container" style="backdrop-filter: blur(5px);z-index:1400">
+		<div id="modalmain-${id}" class="w3-modal-content w3-card-4 w3-animate-zoom mymodal">
+			<div id="modalmainheader-${id}" class="w3-center">
+				<br>
+				<span onclick="closeMainModal('${id}')" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</span>
+				<h5 id="Title-${id}"></h5>
+			</div>
+			<div id="modalBody-${id}">
+			    			     </div>
+
+			<div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+				<button onclick="closeMainModal('${id}')" type="button" class="btn btn-dark btn-block">بازگشت</button>
+			</div>
+		</div>
+	</div>`
+    let modalsDiv = document.getElementById('modals')
+    modalsDiv.innerHTML += modalHtml
+
+    return id
+}
