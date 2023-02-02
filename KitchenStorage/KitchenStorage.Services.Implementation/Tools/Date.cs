d1aed08f0c52af9b65e68e19008506f5e3bcb7a9
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-02 14:54:58 +0330

    create date tools

---
+++ KitchenStorage/KitchenStorage.Services.Implementation/Tools/Date.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace KitchenStorage.Tools;
+
+public static class DateHelper
+{
+    public static Task<string> ToShamsiAsync(this DateTime date)
+    {
+        PersianCalendar pc = new();
+        return Task.FromResult($"{pc.GetYear(date)}/{pc.GetMonth(date)}/{pc.GetDayOfMonth(date)}");
+    }
+
+    public static string ToShamsi(this DateTime date)
+    {
+        PersianCalendar pc = new();
+        return $"{pc.GetYear(date)}/{pc.GetMonth(date)}/{pc.GetDayOfMonth(date)}";
+    }
+}
