07601dff94bff0e176624670906de5c12e992f3d
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-02 19:45:01 +0330

    delete unused pages and middlewares

--- KitchenStorage.Panel/src/router/middleware/auth.ts
+++
@@ -1,8 +0,0 @@
-export default function ({ store, next }: any) {       
-    if (!store.getters["user/auth"].isLogin) {
-        return next({
-            name: "Login"
-        })
-    }
-    return next();
-}
