ac9d2c3d76b518ee52c748d9b066aef90ce54ae4
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-02 14:55:03 +0330

    Update GroupController.cs

--- KitchenStorage/KitchenStorage.Web/Controllers/GroupController.cs
+++ KitchenStorage/KitchenStorage.Web/Controllers/GroupController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KitchenStorage.Services.Abstraction;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KitchenStorage.Web.Controllers;
 
 public class GroupController : Controller
 {
-    public IActionResult Index()
+    private readonly IGroupGet _get;
+
+    private readonly IGroupViewModel _viewModel;
+
+    public GroupController(IGroupGet get, IGroupViewModel viewModel)
     {
-        return View();
+        _get = get;
+        _viewModel = viewModel;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var groups = await _get.GroupsAsync();
+
+        return View(_viewModel.CreateGroupViewModel(groups));
     }
 
     public IActionResult Create()
