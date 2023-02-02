9865631df28035a6b64d60926d34e249b07cca4d
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-02 14:40:43 +0330

    create api models

---
+++ KitchenStorage/KitchenStorage.ViewModel/Api/Api.cs
@@ -0,0 +1,23 @@
+﻿namespace KitchenStorage.ViewModel;
+
+public record ApiModel(int Code, bool Status, string Title, string Message, object? Result);
+
+public record ApiRequest(string Data);
+
+public static class ApiResponse
+{
+    public static string Lang { get; set; } = "fa-ir";
+
+    public static ApiModel Success(string title, string message, object? result)
+        => new(200, true, title, message, result);
+
+    public static ApiModel ApiException(string title = "خطایی رخ داد مجدادا تلاش کنید", string message = "")
+            => new(500, false, title, message, new { });
+
+    public static ApiModel Faild(int code, string title, string message)
+        => new(code, false, title, message, new { });
+}
+
+public record PaginationAndLocation(int Page, int Count, string? Q = "", double Lat = 0, double Lon = 0);
+
+public record PaginationResult<TResult>(int PageCount, TResult Result);
