641153e3b19e3915036bbbff25045fdb0ce60ca0
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-02 13:54:24 +0330

    add property modifies

--- KitchenStorage/KitchenStorage.Entities/Group/Group.cs
+++ KitchenStorage/KitchenStorage.Entities/Group/Group.cs
@@ -4,5 +4,7 @@ namespace KitchenStorage.Entities;
 
 public record Group : BaseEntity
 {
+    [Display(Name = "نام گروه")]
+    [Required(ErrorMessage = "لطفا نام گروه را وارد کنید")]
     public string Name { get; set; }
 }
