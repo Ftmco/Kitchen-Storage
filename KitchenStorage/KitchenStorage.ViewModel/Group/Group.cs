253508c681d1a6c1719d65878463e2e7036b1351
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-02 14:41:02 +0330

    change group view models

--- KitchenStorage/KitchenStorage.ViewModel/Group/Group.cs
+++ KitchenStorage/KitchenStorage.ViewModel/Group/Group.cs
@@ -1,5 +1,12 @@
 ﻿namespace KitchenStorage.ViewModel;
 
-public record GroupViewModel(Guid Id, string Name, string CreateDate);
+public record GroupViewModel(Guid Id, string Name, string CreateDate, byte Status);
 
 public record UpsertGroupViewModel(Guid? Id, string Name);
+
+public enum GroupActionStatus
+{
+    Success = 0,
+    Failed = 1,
+    NotFound = 2
+}
