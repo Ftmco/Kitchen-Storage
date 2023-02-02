ce70f85895c8268c8138ed021ad659c96f3ad188
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-02 13:54:39 +0330

    create group api

---
+++ KitchenStorage/KitchenStorage.Web/Api Controllers/GroupController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KitchenStorage.Web.ApiControllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GroupController : ControllerBase
+{
+    [HttpPost("Upsert")]
+    public async Task<IActionResult> UpsertAsync(UpsertGroupViewModel upsert)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("Delete")]
+    public async Task<IActionResult> DeleteAsync(Guid id)
+    {
+        return Ok();
+    }
+}
