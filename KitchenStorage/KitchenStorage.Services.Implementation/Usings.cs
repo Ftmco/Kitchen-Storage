c0590cf3208ae573b9eec93a0e539451eb1a20c1
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-02 14:55:05 +0330

    Update Usings.cs

--- KitchenStorage/KitchenStorage.Services.Implementation/Usings.cs
+++ KitchenStorage/KitchenStorage.Services.Implementation/Usings.cs
@@ -1,4 +1,5 @@
 ﻿global using KitchenStorage.Entities;
 global using KitchenStorage.Services.Abstraction;
 global using KitchenStorage.Services.Abstraction.Base;
+global using KitchenStorage.Tools;
 global using KitchenStorage.ViewModel;
