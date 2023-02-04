8cdaa3a725b6283539c8caee110243bb67e066fa
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-04 22:51:42 +0330

    Update IMeasurementTypeAction.cs

--- KitchenStorage/KitchenStorage.Services.Abstraction/MeasurmentType/IMeasurementTypeAction.cs
+++ KitchenStorage/KitchenStorage.Services.Abstraction/MeasurmentType/IMeasurementTypeAction.cs
@@ -1,13 +1,12 @@
-﻿namespace KitchenStorage.Services.Abstraction
+﻿namespace KitchenStorage.Services.Abstraction;
+
+public interface IMeasurementTypeAction
 {
-    public interface IMeasurementTypeAction
-    {
-        Task<Either<MeasurementTypeActionStatus, MeasurementType>> UpsertAsync(UpsertMeasurementTypeViewModel upsert);
+    Task<Either<MeasurementTypeActionStatus, MeasurementType>> UpsertAsync(UpsertMeasurementTypeViewModel upsert);
 
-        Task<Either<MeasurementTypeActionStatus, MeasurementType>> CreateAsync(UpsertMeasurementTypeViewModel upsert);
+    Task<Either<MeasurementTypeActionStatus, MeasurementType>> CreateAsync(UpsertMeasurementTypeViewModel upsert);
 
-        Task<Either<MeasurementTypeActionStatus, MeasurementType>> UpdateAsync(UpsertMeasurementTypeViewModel upsert);
+    Task<Either<MeasurementTypeActionStatus, MeasurementType>> UpdateAsync(UpsertMeasurementTypeViewModel upsert);
 
-        Task<MeasurementTypeActionStatus> DeleteAsync(Guid id);
-    }
+    Task<MeasurementTypeActionStatus> DeleteAsync(Guid id);
 }
