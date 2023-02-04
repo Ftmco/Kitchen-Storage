bc1e0cab5de000cbd2ec72b5f9f313ece225ffd3
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-04 22:51:28 +0330

    add new fields for inventory

--- KitchenStorage/KitchenStorage.Entities/Inventory/Inventory.cs
+++ KitchenStorage/KitchenStorage.Entities/Inventory/Inventory.cs
@@ -11,9 +11,21 @@ public record Inventory : BaseEntity
     public double Value { get; set; }
 
     [Required]
-    public int TypeId { get; set; }
-    public virtual MeasurementType MeasurementType { get; set; }
+    public double AlarmValue { get; set; }
+
+    [Required]
+    public Guid TypeId { get; set; }
 
     [Required]
     public string Description { get; set; }
+
+    public virtual MeasurementType MeasurementType { get; set; }
+
+}
+
+public enum InventoryStatus
+{
+    Available = 0,
+    AlertLimit = 1,
+    Finished = 2,
 }
