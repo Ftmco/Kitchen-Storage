1864bf7e8efff00b615f9938af48858d3c0de63f
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-04 12:38:16 +0330

    Update FoodController.cs

--- KitchenStorage/KitchenStorage.Web/KitchenStorage.Api/Controllers/FoodController.cs
+++ KitchenStorage/KitchenStorage.Web/KitchenStorage.Api/Controllers/FoodController.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace KitchenStorage.Api.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FoodController : ControllerBase
-    {
+namespace KitchenStorage.Api.Controllers;
 
-    }
+[Route("api/[controller]")]
+[ApiController]
+public class FoodController : ControllerBase
+{
+
 }
