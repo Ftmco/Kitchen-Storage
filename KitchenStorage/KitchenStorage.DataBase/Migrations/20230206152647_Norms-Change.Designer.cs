c88f3101fe0e57c37da0ff1f80f578f556887016
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-06 19:01:49 +0330

    update data base

--- KitchenStorage/KitchenStorage.DataBase/Migrations/20230206152647_Norms-Change.Designer.cs
+++ KitchenStorage/KitchenStorage.DataBase/Migrations/20230206152647_Norms-Change.Designer.cs
@@ -4,6 +4,7 @@ using KitchenStorage.DataBase.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KitchenStorage.DataBase.Migrations
 {
     [DbContext(typeof(KitchenContext))]
-    partial class KitchenContextModelSnapshot : ModelSnapshot
+    [Migration("20230206152647_Norms-Change")]
+    partial class NormsChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ KitchenStorage/KitchenStorage.DataBase/Migrations/20230206152647_Norms-Change.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KitchenStorage.DataBase.Migrations
+{
+    public partial class NormsChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Norms");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Norms",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
