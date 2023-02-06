e67c612f7c971c3e8714c8dcd86432e38bf2ea8a
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-06 13:02:57 +0330

    change inventory service to implement type of invetories

--- KitchenStorage/KitchenStorage.Services.Implementation/Inventory/InventoryViewModelService.cs
+++ KitchenStorage/KitchenStorage.Services.Implementation/Inventory/InventoryViewModelService.cs
@@ -1,17 +1,38 @@
-﻿namespace KitchenStorage.Services.Implementation
-{
-    public class InventoryViewModelService : IInventoryViewModel
-    {
-        public InventoryViewModel CreateInventoryViewModel(Inventory inventory)
-             => new(Id: inventory.Id,
-                 Name: inventory.Name,
-                 Value: inventory.Value,
-                 AlertLimit: inventory.AlertLimit,
-                 TypeId: inventory.TypeId,
-                 Description: inventory.Description,
-                 CreateDate: inventory.CreateDate.ToShamsi());
+﻿namespace KitchenStorage.Services.Implementation;
 
-        public IEnumerable<InventoryViewModel> CreateInventoryViewModel(IEnumerable<Inventory> inventories)
-            => inventories.Select(CreateInventoryViewModel);
+public class InventoryViewModelService : IInventoryViewModel
+{
+    private readonly IBaseQuery<MeasurementType> _typeQuery;
+
+    private readonly IMeasurementTypeViewModel _typeViewModel;
+
+    public InventoryViewModelService(IBaseQuery<MeasurementType> typeQuery, IMeasurementTypeViewModel typeViewModel)
+    {
+        _typeQuery = typeQuery;
+        _typeViewModel = typeViewModel;
+    }
+
+    public async Task<InventoryViewModel> CreateInventoryViewModelAsync(Inventory inventory)
+    {
+        var type = await _typeQuery.GetAsync(inventory.TypeId);
+
+        return new(Id: inventory.Id,
+             Name: inventory.Name,
+             Value: inventory.Value,
+             AlertLimit: inventory.AlertLimit,
+             Description: inventory.Description,
+             CreateDate: inventory.CreateDate.ToShamsi(),
+             Status: inventory.Status,
+             Type: type is null ? null : _typeViewModel.CreateMeasurementTypeViewModel(type));
+    }
+
+    public async Task<IEnumerable<InventoryViewModel>> CreateInventoryViewModelAsync(IEnumerable<Inventory> inventories)
+    {
+        List<InventoryViewModel> viewModels = new();
+
+        foreach (var item in inventories)
+            viewModels.Add(await CreateInventoryViewModelAsync(item));
+
+        return viewModels;
     }
 }
