ce2f8a4d27726af7ba8da351eb94712fe8b9b808
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-08 13:08:31 +0330

    fix git conflict

--- KitchenStorage/KitchenStorage.Entities/Food/Norm.cs
+++ KitchenStorage/KitchenStorage.Entities/Food/Norm.cs
@@ -1,6 +1,4 @@
-﻿using KitchenStorage.Entities.Base;
-
-namespace KitchenStorage.Entities;
+﻿namespace KitchenStorage.Entities;
 
 public record Norm : BaseEntity
 {
@@ -10,6 +8,16 @@ public record Norm : BaseEntity
     [Required]
     public double Value { get; set; }
 
+    [Required]
+    public Guid FoodId { get; set; }
+
+    [Required]
     public Guid InventoryId { get; set; }
+
+    // Navigation Properties
+    // Relationships
+
     public virtual Inventory Inventory { get; set; }
+
+    public virtual Food Food { get; set; }
 }
