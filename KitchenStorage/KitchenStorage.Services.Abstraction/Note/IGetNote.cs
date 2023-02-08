6e92ea9cd211dc98b165e7e54f93516c4a2d590c
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-08 13:09:42 +0330

    remove unused functions

--- KitchenStorage/KitchenStorage.Services.Abstraction/Note/IGetNote.cs
+++ KitchenStorage/KitchenStorage.Services.Abstraction/Note/IGetNote.cs
@@ -1,9 +1,6 @@
-﻿namespace KitchenStorage.Services.Abstraction
+﻿namespace KitchenStorage.Services.Abstraction;
+
+public interface IGetNote
 {
-    public interface IGetNote
-    {
-        Task<PaginationResult<IEnumerable<Note>>> NotesAsync(int page, int pageCount);
-        Task<IEnumerable<Note>> NotesAsync();
-        Task<Note?> FindByIdAsync(Guid id);
-    }
+    Task<PaginationResult<IEnumerable<Note>>> NotesAsync(int page, int pageCount);
 }
--- KitchenStorage/KitchenStorage.Services.Implementation/Note/GetNote.cs
+++ KitchenStorage/KitchenStorage.Services.Implementation/Note/GetNote.cs
@@ -1,31 +1,20 @@
-﻿using KitchenStorage.Entities.Base;
-using KitchenStorage.Services.Implementation.Tools;
-using System.Linq.Expressions;
+﻿namespace KitchenStorage.Services.Implementation;
 
-namespace KitchenStorage.Services.Implementation
+public class GetNote : IGetNote
 {
-    public class GetNote : IGetNote
+    private readonly IBaseQuery<Note> _noteQuery;
+
+    public GetNote(IBaseQuery<Note> query)
     {
-        private readonly IBaseQuery<Note> _noteQuery;
+        _noteQuery = query;
+    }
 
-        public GetNote(IBaseQuery<Note> query)
-        {
-            _noteQuery = query;
-        }
 
-        public async Task<Note?> FindByIdAsync(Guid id)
-            => await _noteQuery.GetAsync(id);
+    public async Task<PaginationResult<IEnumerable<Note>>> NotesAsync(int page, int pageCount)
+    {
+        IEnumerable<Note> notes = await _noteQuery.GetAllAsync(page, pageCount);
+        var notesCount = await _noteQuery.CountAsync();
 
-        public async Task<PaginationResult<IEnumerable<Note>>> NotesAsync(int page, int pageCount)
-        {
-            IEnumerable<Note> notes = await _noteQuery.GetAllAsync(page, pageCount);
-            var notesCount = await _noteQuery.CountAsync();
-
-            return notes.GetPaginationResult(notesCount.PageCount(pageCount));
-        }
-
-        public async Task<IEnumerable<Note>> NotesAsync()
-            => await _noteQuery.GetAllAsync();
-
+        return notes.GetPaginationResult(notesCount.PageCount(pageCount));
     }
 }
