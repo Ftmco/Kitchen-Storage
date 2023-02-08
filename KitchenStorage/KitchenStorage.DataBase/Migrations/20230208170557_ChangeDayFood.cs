f50f6242602052a953c46e5874fab13fb356576c
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-08 20:54:31 +0330

    change relationships with day foods and update data base

---
+++ KitchenStorage/KitchenStorage.DataBase/Migrations/20230208170557_ChangeDayFood.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KitchenStorage.DataBase.Migrations
+{
+    public partial class ChangeDayFood : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DaysFoods");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Meal",
+                table: "DayFood",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "FoodId",
+                table: "DayFood",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FoodId",
+                table: "DayFood");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Meal",
+                table: "DayFood",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "DaysFoods",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DayFoodId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FoodId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DaysFoods", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DaysFoods_DayFood_DayFoodId",
+                        column: x => x.DayFoodId,
+                        principalTable: "DayFood",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DaysFoods_Foods_FoodId",
+                        column: x => x.FoodId,
+                        principalTable: "Foods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DaysFoods_DayFoodId",
+                table: "DaysFoods",
+                column: "DayFoodId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DaysFoods_FoodId",
+                table: "DaysFoods",
+                column: "FoodId");
+        }
+    }
+}
