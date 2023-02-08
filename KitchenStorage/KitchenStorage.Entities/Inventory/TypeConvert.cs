8fa01f38ced53d2aca2376fb92d447390c971f06
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-08 13:08:40 +0330

    create type convert table

---
+++ KitchenStorage/KitchenStorage.Entities/Inventory/TypeConvert.cs
@@ -0,0 +1,17 @@
+﻿namespace KitchenStorage.Entities;
+
+[Index(nameof(FromTypeId), nameof(ToTypeId))]
+public record TypeConvert : BaseEntity
+{
+    [Required]
+    public Guid FromTypeId { get; set; }
+
+    [Required]
+    public Guid ToTypeId { get; set; }
+
+    [Required]
+    public double FromValue { get; set; }
+
+    [Required]
+    public double ToValue { get; set; }
+}
