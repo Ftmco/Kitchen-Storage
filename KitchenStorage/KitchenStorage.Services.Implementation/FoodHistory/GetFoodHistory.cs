593ee34cdb437284577850106d7e9f53839cb5b6
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-10 15:19:43 +0330

    fix code

--- KitchenStorage/KitchenStorage.Services.Implementation/FoodHistory/GetFoodHistory.cs
+++ KitchenStorage/KitchenStorage.Services.Implementation/FoodHistory/GetFoodHistory.cs
@@ -1,22 +1,21 @@
-﻿namespace KitchenStorage.Services.Implementation
+﻿namespace KitchenStorage.Services.Implementation;
+
+public class GetFoodHistory : IGetFoodHistory
 {
-    public class GetFoodHistory : IGetFoodHistory
+    private readonly IBaseQuery<FoodHistory> _query;
+
+    public GetFoodHistory(IBaseQuery<FoodHistory> query)
     {
-        private readonly IBaseQuery<FoodHistory> _query;
-
-        public GetFoodHistory(IBaseQuery<FoodHistory> query)
-        {
-            _query = query;
-        }
-
-        public async Task<PaginationResult<IEnumerable<FoodHistory>>> FoodHistoriesAsync(int page, int count)
-        {
-            IEnumerable<FoodHistory> foodHistories = await _query.GetAllAsync(page, count, x => x.CreateDate);
-            var foodHistoriesCount = await _query.CountAsync();
-
-            return foodHistories.GetPaginationResult(foodHistoriesCount.PageCount(count));
-        }
-        public async Task<IEnumerable<FoodHistory>> FoodHistoriesAsync()
-            => await _query.GetAllAsync();
+        _query = query;
     }
+
+    public async Task<PaginationResult<IEnumerable<FoodHistory>>> FoodHistoriesAsync(int page, int count)
+    {
+        IEnumerable<FoodHistory> foodHistories = await _query.GetAllAsync(page, count, x => x.CreateDate);
+        var foodHistoriesCount = await _query.CountAsync();
+
+        return foodHistories.GetPaginationResult(foodHistoriesCount.PageCount(count));
+    }
+    public async Task<IEnumerable<FoodHistory>> FoodHistoriesAsync()
+        => await _query.GetAllAsync();
 }
--- KitchenStorage/KitchenStorage.Web/KitchenStorage.Api/Controllers/FoodHistoryController.cs
+++ KitchenStorage/KitchenStorage.Web/KitchenStorage.Api/Controllers/FoodHistoryController.cs
@@ -1,31 +1,27 @@
-﻿using KitchenStorage.Services.Abstraction;
-using Microsoft.AspNetCore.Mvc;
+﻿namespace KitchenStorage.Api.Controllers;
 
-namespace KitchenStorage.Api.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class FoodHistoryController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FoodHistoryController : ControllerBase
+    private readonly IGetFoodHistory _query;
+    private readonly IFoodHistoryViewModel _viewModel;
+
+    public FoodHistoryController(IGetFoodHistory query,
+        IFoodHistoryViewModel viewModel)
     {
-        private readonly IGetFoodHistory _query;
-        private readonly IFoodHistoryViewModel _viewModel;
+        _query = query;
+        _viewModel = viewModel;
+    }
 
-        public FoodHistoryController(IGetFoodHistory query,
-            IFoodHistoryViewModel viewModel)
+    [HttpGet("FoodHistories")]
+    public async Task<IActionResult> Get(int page, int count)
+    {
+        var history = await _query.FoodHistoriesAsync(page, count);
+        return Ok(Success("", "", new
         {
-            _query = query;
-            _viewModel = viewModel;
-        }
-
-        [HttpGet("FoodHistories")]
-        public async Task<IActionResult> Get(int page, int count)
-        {
-            var history = await _query.FoodHistoriesAsync(page, count);
-            return Ok(Success("", "", new
-            {
-                history.PageCount,
-                History = await _viewModel.CreateFoodHistoryViewModel(history.Result),
-            }));
-        }
+            history.PageCount,
+            History = await _viewModel.CreateFoodHistoryViewModel(history.Result),
+        }));
     }
 }
