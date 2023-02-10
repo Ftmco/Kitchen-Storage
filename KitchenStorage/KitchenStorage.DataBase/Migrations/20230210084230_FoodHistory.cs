49f72fedf21b027abb31a2c52d78458f820b7dc5
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-10 12:14:06 +0330

    update data base

---
+++ KitchenStorage/KitchenStorage.DataBase/Migrations/20230210084230_FoodHistory.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KitchenStorage.DataBase.Migrations
+{
+    public partial class FoodHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Meal",
+                table: "FoodHistories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DayId",
+                table: "FoodHistories",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "FoodHistories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FoodHistories_DayId",
+                table: "FoodHistories",
+                column: "DayId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FoodHistories_Day_DayId",
+                table: "FoodHistories",
+                column: "DayId",
+                principalTable: "Day",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FoodHistories_Day_DayId",
+                table: "FoodHistories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FoodHistories_DayId",
+                table: "FoodHistories");
+
+            migrationBuilder.DropColumn(
+                name: "DayId",
+                table: "FoodHistories");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "FoodHistories");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Meal",
+                table: "FoodHistories",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
