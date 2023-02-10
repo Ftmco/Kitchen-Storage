831755d1d786f1dd3509c1f2ba17a7e173248069
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-10 13:27:17 +0330

    Update Food.cs

--- KitchenStorage/KitchenStorage.ViewModel/Food/Food.cs
+++ KitchenStorage/KitchenStorage.ViewModel/Food/Food.cs
@@ -1,23 +1,21 @@
-﻿namespace KitchenStorage.ViewModel
+﻿namespace KitchenStorage.ViewModel;
+
+public record FoodViewModel
+    (Guid? Id, string Name, byte Type, byte Status, string CreateDate);
+
+public record UpsertFoodViewModel
+     (Guid? Id, string Name, byte Type);
+
+public enum FoodType
 {
-
-    public record FoodViewModel
-        (Guid? Id, string Name, byte Type, byte Status, string CreateDate);
-
-    public record UpsertFoodViewModel
-         (Guid? Id, string Name, byte Type);
-
-    public enum FoodType
-    {
-        Soldier = 0,
-        Employee = 1
-    }
-
-    public enum FoodActionStatus
-    {
-        Success = 0,
-        Failed = 1,
-        NotFound = 2
-    }
-
+    Soldier = 0,
+    Employee = 1
 }
+
+public enum FoodActionStatus
+{
+    Success = 0,
+    Failed = 1,
+    NotFound = 2
+}
+
