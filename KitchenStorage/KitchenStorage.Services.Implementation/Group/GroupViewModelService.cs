f9c7fac4a3eb27940aa55e106c11086699dd95fc
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-12 21:08:48 +0330

    update data base

--- KitchenStorage/KitchenStorage.Services.Implementation/Group/GroupViewModelService.cs
+++ KitchenStorage/KitchenStorage.Services.Implementation/Group/GroupViewModelService.cs
@@ -5,8 +5,7 @@ internal class GroupViewModelService : IGroupViewModel
     public GroupViewModel CreateGroupViewModel(Group group)
         => new(Id: group.Id,
             Name: group.Name,
-            CreateDate: group.CreateDate.ToShamsi(),
-            Status: group.Status);
+            CreateDate: group.CreateDate.ToShamsi());
 
     public IEnumerable<GroupViewModel> CreateGroupViewModel(IEnumerable<Group> groups)
         => groups.Select(CreateGroupViewModel);
