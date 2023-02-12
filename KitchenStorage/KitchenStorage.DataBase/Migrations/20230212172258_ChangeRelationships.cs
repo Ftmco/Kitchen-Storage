1ad988e80b8aa6bb79b9235106fa06c70ac0a220
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-12 20:53:16 +0330

    update data base

---
+++ KitchenStorage/KitchenStorage.DataBase/Migrations/20230212172258_ChangeRelationships.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KitchenStorage.DataBase.Migrations
+{
+    public partial class ChangeRelationships : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inventories_Group_GroupId",
+                table: "Inventories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Inventories_GroupId",
+                table: "Inventories");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_GroupId",
+                table: "Inventories",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inventories_Group_GroupId",
+                table: "Inventories",
+                column: "GroupId",
+                principalTable: "Group",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
