6a30850cf81e69a3bb7e4baf2426af24d6090f71
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-12 22:05:25 +0330

    Update FoodHistory.cs

--- KitchenStorage/KitchenStorage.ViewModel/Food/FoodHistory.cs
+++ KitchenStorage/KitchenStorage.ViewModel/Food/FoodHistory.cs
@@ -1,16 +1,15 @@
-﻿namespace KitchenStorage.ViewModel
+﻿namespace KitchenStorage.ViewModel;
+
+public record FoodHistoryViewModel
+(Guid Id, DayViewModel? Day, FoodViewModel? Food, int Count, string Meal, string Description, string CreateDate);
+
+public record InventoryHistoryViewModel(Guid Id, string Description, string CreateDate, IEnumerable<InventoryHistoryListViewModel> HistoryList);
+
+public record InventoryHistoryListViewModel(Guid Id, Guid HistoryId, double Value, double InventoryValue, InventoryPreviewViewModel? Inventory);
+
+public enum FoodHistoryActionStatus
 {
-    public record FoodHistoryViewModel
-    (Guid? Id, DayViewModel Day, FoodViewModel Food, int Count, string Meal, string Description, string CreateDate);
-
-    public record UpsertFoodHistoryViewModel
-         (Guid? Id, Guid DayId, Guid FoodId, int Count, string Meal, string Description);
-
-
-    public enum FoodHistoryActionStatus
-    {
-        Success = 0,
-        Failed = 1,
-        NotFound = 2
-    }
+    Success = 0,
+    Failed = 1,
+    NotFound = 2
 }
