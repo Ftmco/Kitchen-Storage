7d52534d86a6a9aa2e49e17924ea0560a1a39316
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-12 22:34:13 +0330

    Update FoodHistory.cs

--- KitchenStorage/KitchenStorage.ViewModel/Food/FoodHistory.cs
+++ KitchenStorage/KitchenStorage.ViewModel/Food/FoodHistory.cs
@@ -3,7 +3,7 @@
 public record FoodHistoryViewModel
 (Guid Id, DayViewModel? Day, FoodViewModel? Food, int Count, string Meal, string Description, string CreateDate);
 
-public record InventoryHistoryViewModel(Guid Id, string Description, string CreateDate, IEnumerable<InventoryHistoryListViewModel> HistoryList);
+public record InventoryHistoryViewModel(Guid Id, string Description, string CreateDate, byte Type, IEnumerable<InventoryHistoryListViewModel> HistoryList);
 
 public record InventoryHistoryListViewModel(Guid Id, Guid HistoryId, double Value, double InventoryValue, InventoryPreviewViewModel? Inventory);
 
