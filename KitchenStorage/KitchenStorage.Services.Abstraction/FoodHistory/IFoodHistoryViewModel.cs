dd3052709604b0f9b29ea0a74cb848f7f6b2cb9e
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-12 22:05:02 +0330

    delete food histories

--- KitchenStorage/KitchenStorage.Services.Abstraction/FoodHistory/IFoodHistoryViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KitchenStorage.Services.Abstraction
-{
-    public interface IFoodHistoryViewModel
-    {
-        Task<FoodHistoryViewModel> CreateFoodHistoryViewModel(FoodHistory foodHistory);
-
-        Task<IEnumerable<FoodHistoryViewModel>> CreateFoodHistoryViewModel(IEnumerable<FoodHistory> foodHistories);
-    }
-}
